3482268e7012d926db50556500f33210d9ae4704
Lopsang Lama <lopsang.hyolmopa@gmail.com>
2024-08-26 23:18:51 +0545

    add RouterProvider from react-router-dom

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,7 +1,7 @@
+
 function App() {
   return (
     <>
-      <h1 className="text-5xl">React Router</h1>
     </>
   );
 }
--- react-router/src/main.tsx
+++ react-router/src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+import Layout from "./Layout.tsx";
+import Home from "./components/Home.tsx";
+import About from "./components/About.tsx";
+import Contact from "./components/Contact.tsx";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <RouterProvider router={appRouter} />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
