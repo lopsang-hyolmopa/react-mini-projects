377ecdbe3351391d0f1d07774d1f2aa25b7b72b0
Lopsang Lama <lopsang.hyolmopa@gmail.com>
2024-09-01 22:09:53 +0545

    add TodoItem component

--- todo-context-localstorage/src/components/TodoItem.tsx
+++ todo-context-localstorage/src/components/TodoItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { useTodo } from "../contexts";
 import { TodoType } from "../types";
 
@@ -6,7 +8,19 @@ type Props = {
 };
 
 function TodoItem({ todo }: Props) {
-  const { deleteTodo } = useTodo();
+  const [updatedTodoTitle, setUpdatedTodoTitle] = useState<string>(todo.title);
+  const [isTodoEditable, setIsTodoEditable] = useState<boolean>(false);
+
+  const { updateTodo, deleteTodo, toogleComplete } = useTodo();
+
+  const toggleCompleted = () => {
+    toogleComplete(todo.id);
+  };
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, title: updatedTodoTitle });
+  };
+
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
@@ -17,30 +31,30 @@ function TodoItem({ todo }: Props) {
         type="checkbox"
         className="cursor-pointer"
         checked={todo.isCompleted}
-        // onChange={toggleCompleted}
+        onChange={toggleCompleted}
       />
-      {/* <input
+      <input
         type="text"
         className={`border outline-none w-full bg-transparent rounded-lg ${
           isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-        } ${todo.completed ? "line-through" : ""}`}
-        value={todoMsg}
-        onChange={(e) => setTodoMsg(e.target.value)}
+        } ${todo.isCompleted ? "line-through" : ""}`}
+        value={updatedTodoTitle}
+        onChange={(e) => setUpdatedTodoTitle(e.target.value)}
         readOnly={!isTodoEditable}
-      /> */}
+      />
       {/* Edit, Save Button */}
       <button
         className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
-        // onClick={() => {
-        //   if (todo.completed) return;
+        onClick={() => {
+          if (todo.isCompleted) return;
 
-        //   if (isTodoEditable) {
-        //     editTodo();
-        //   } else setIsTodoEditable((prev) => !prev);
-        // }}
-        // disabled={todo.completed}
+          if (isTodoEditable) {
+            editTodo();
+          } else setIsTodoEditable((prev) => !prev);
+        }}
+        disabled={todo.isCompleted}
       >
-        {/* {isTodoEditable ? "📁" : "✏️"} */}
+        {isTodoEditable ? "📁" : "✏️"}
       </button>
       {/* Delete Todo Button */}
       <button
